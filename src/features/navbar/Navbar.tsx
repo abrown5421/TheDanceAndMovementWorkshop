717d5ab0504654723ccd2940eb46e1b74ebd11d6
abrown5421 <augustashtonbrown@gmail.com>
2025-05-13 18:32:06 -0400

    adding the navbar component back in as a feature slice

---
+++ src/features/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+import Transition from '../../components/transition/Transition';
+import './navbar.css';
+
+function Navbar() {
+  
+    return (
+      <div className="bg-white flex flex-row justify-between items-center py-2 px-4 min-h-14 relative navbar">
+        <div className="flex flex-col max-h-full">
+            <Transition entry="animate__fadeInLeft" exit="animate__fadeOutLeft" isEntering={true}>
+                logo here
+            </Transition>
+        </div>
+        <div className="flex flex-col">
+            <Transition entry="animate__fadeInRight" exit="animate__fadeOutRight" isEntering={true}>
+                Menu here
+            </Transition>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Navbar;
+  
