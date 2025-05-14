6a181b2bccef7458baeed3b9aec8ca15e955fa08
abrown5421 <augustashtonbrown@gmail.com>
2025-05-13 21:33:22 -0400

    restoring the logo in the navbar

--- src/features/navbar/Navbar.tsx
+++ src/features/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import Transition from '../../components/transition/Transition';
+import Logo from './Logo';
 import './navbar.css';
 
 function Navbar() {
@@ -7,7 +8,7 @@ function Navbar() {
       <div className="bg-white flex flex-row justify-between items-center py-2 px-4 min-h-14 relative navbar">
         <div className="flex flex-col max-h-full">
             <Transition entry="animate__fadeInLeft" exit="animate__fadeOutLeft" isEntering={true}>
-                logo here
+                <Logo />
             </Transition>
         </div>
         <div className="flex flex-col">
