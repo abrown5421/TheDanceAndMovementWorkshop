2dff88a54b6f6428da77d1ed076bae633537d465
abrown5421 <augustashtonbrown@gmail.com>
2025-05-23 12:34:52 -0400

    adding in a loop over auxilary menu items in the footer and adding in a KVP to the pageNavConfig field on the db so we can allow the user to specify if theyd like a link present in the auxilary nav

--- src/features/footer/Footer.tsx
+++ src/features/footer/Footer.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Menu from '../navbar/Menu';
 import './footer.css'
 import { getCurrentYear } from '../../utils/getCurrentYear';
 import Block from '../../components/block/Block';
 import SocialBar from '../socialBar/SocialBar';
+import { useAppSelector } from '../../app/store/hooks';
+import Transition from '../../components/transition/Transition';
+import clsx from 'clsx';
+import { useNavigationHook } from '../../hooks/NavigationHook';
 
 const Footer: React.FC = () => {
-
+  const handleNavigation = useNavigationHook()
+  const pages = useAppSelector((state) => state.pages.pages.filter((page) => page.PageNavConfig.Aux))
+  
   return (
     <Block tailwindClasses="bg-white flex flex-col sm:flex-row justify-between items-center min-h-14 relative footer z-40">
-        <Block tailwindClasses="flex flex-col justify-around h-full w-full">
+        <Block tailwindClasses="flex flex-col justify-evenly h-full w-full">
             <Block tailwindClasses='block sm:hidden font-primary flex flex-col justify-center items-center p-5'>
                 <Block tailwindClasses='flex flex-row'></Block>
                 <Block tailwindClasses='flex flex-row flex-wrap-nowrap'>
@@ -19,14 +25,36 @@ const Footer: React.FC = () => {
                 </Block>
             </Block>
             <Menu />
-            <Block tailwindClasses='hidden sm:block px-5'>
-                © {getCurrentYear()} The Dance and Movement Workshop. All rights reserved.
+            <Block tailwindClasses='hidden sm:flex px-5 flex-row'>
+                © {getCurrentYear()} The Dance and Movement Workshop. All rights reserved. 
+                {pages.length > 0 && <div className='mx-2'>|</div>}
+                {pages.map((page) => (
+                    <Transition key={page.PageID} delay={100 * page.PageNavConfig.Order} isEntering={true} tailwindClass='cursor-pointer'>
+                        <div
+                            className={clsx(page.PageActive ? "hover:text-primary text-black" : "display-none")}
+                            onClick={() => handleNavigation(page.PageSlug, page.PageName, page.PageID)}
+                        >
+                            {page.PageName}
+                        </div>
+                    </Transition>
+                ))}
             </Block>
         </Block>
         <Block tailwindClasses="flex flex-col max-h-full">
             <SocialBar />
-            <Block tailwindClasses='block sm:hidden p-5'>
+            <Block tailwindClasses='flex sm:hidden p-5 flex-row'>
                 © {getCurrentYear()} The Dance and Movement Workshop. All rights reserved.
+                {pages.length > 0 && <div className='mx-2'>|</div>}
+                {pages.map((page) => (
+                    <Transition key={page.PageID} delay={100 * page.PageNavConfig.Order} isEntering={true} tailwindClass='cursor-pointer'>
+                        <div
+                            className={clsx(page.PageActive ? "hover:text-primary text-black" : "display-none")}
+                            onClick={() => handleNavigation(page.PageSlug, page.PageName, page.PageID)}
+                        >
+                            {page.PageName}
+                        </div>
+                    </Transition>
+                ))}
             </Block>
         </Block>
     </Block>
--- src/features/pages/pagesTypes.ts
+++ src/features/pages/pagesTypes.ts
@@ -11,6 +11,7 @@ export interface PagesState {
   PageNavConfig: {
     Order: number;
     Show: boolean;
+    Aux: boolean;
   };
 }
 
